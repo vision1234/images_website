d440537b0a5ffe7f36d382325d7c43fedc31ab0f
unknown <709318017@qq.com>
2023-07-31 23:25:02 +0800

    upload

---
+++ upload.py
@@ -0,0 +1,46 @@
+import os
+from flask import Flask, render_template, request
+import time
+
+app = Flask(__name__)
+
+# 设置文件上传的目录
+app.config['UPLOAD_FOLDER'] = 'd:/'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # 获取上传的文件和表单数据
+        file = request.files['file']
+        directory1 = request.form['directory1']
+        directory2 = request.form['directory2']
+        directory3 = request.form['directory3']
+
+        print(os.path.join(app.config['UPLOAD_FOLDER'], directory1, directory2, directory3))
+        # 检查是否选择了三级目录
+        # if not directory1 or not directory2 or not directory3:
+        #     return '请填写完整的目录信息！'
+
+        # 创建目录（如果不存在）
+        target_directory = os.path.join(app.config['UPLOAD_FOLDER'], directory1, directory2, directory3)
+        os.makedirs(target_directory, exist_ok=True)
+
+
+        # Check if a file with the same name exists in the target directory
+        filename, file_extension = os.path.splitext(file.filename)
+        timestamp = str(int(time.time()))
+        target_file_path = os.path.join(target_directory, file.filename)
+        while os.path.exists(target_file_path):
+            file.filename = f"{filename}_{timestamp}{file_extension}"
+            target_file_path = os.path.join(target_directory, file.filename)
+
+        file.save(target_file_path)
+
+        return '文件上传成功！'
+
+    return render_template('upload.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
